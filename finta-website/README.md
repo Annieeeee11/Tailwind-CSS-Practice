f232543c8c374a5744c62f681f00aabac4b92d89
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-12-26 00:46:58 +0530

    .

--- finta-website/README.md
+++
@@ -1,3 +0,0 @@
-# Basic html 
-## This is basic HTML practice code that I wrote while learning from Manu Paji’s Tailwind CSS series.
-![Screenshot](public/image.png)
--- tailwind-practice/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Hero } from "@/components/hero";
-
-export default function Home() {
-  return (
-    <div className="h-screen bg-neutral-950 bg-[radial-gradient(80%_50%_at_50%_-20%,rgba(94,176,239,.5),transparent)]">
-      <Hero />
-    </div>
-  );
-}
