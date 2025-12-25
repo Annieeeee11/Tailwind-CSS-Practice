dca3a13eed79a0e5da516139fd7a398879e1ac27
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-12-26 00:42:53 +0530

    basic html practice code

---
+++ 1-finta-website/app/page.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/container";
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <div className="layout">
+      <Container>
+        <div className="left-line" />
+        <div className="right-line" />
+        <Navbar />
+        <Hero />
+      </Container>
+    </div>
+  );
+}
