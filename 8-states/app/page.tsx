e3099c336295de2b25a4a136d8bd496c46844dd3
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-12-26 00:45:47 +0530

    core concepts

---
+++ 8-states/app/page.tsx
@@ -0,0 +1,10 @@
+import Forms from "@/components/forms";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-screen items-center mx-auto max-w-4xl">
+      <Forms />
+    </main>
+  );
+}
